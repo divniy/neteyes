8e5abae0f61e326ff664ac22d6280a73d50f6321
Dmitriy Paramonov <dev@divniy.com>
2012-08-06 12:29:28 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neteyes::Application.config.secret_token = '85a074e1fad8e328b9264278041e99672545b7b1ea18aaec2adeab82c725440b6d102a085daec0475b0ddaa43ca8f19c3517c8560424299a4b0a71640c5b902b'
