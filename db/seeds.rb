8b2c5eaa937063774f54955eb96383b8c4851441
Dmitriy Paramonov <dev@divniy.com>
2012-08-07 00:58:30 +0400

    Scaffold Desk

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require "factory_girl"
-require "ffaker"
-Rake::Task['db:reset'].invoke
 
-FactoryGirl.create_list(:desk, 4)
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   sequence :options do |n|
-    rand(3..3).times.inject({}) {|hash,x| hash.merge "stream#{x}".to_sym => Faker::Address.street_name.capitalize }
+    rand(3..3).times.inject({}) {|hash,x| hash.merge "stream-#{x}".to_sym => Faker::Address.street_name.capitalize }
   end
 
   factory :desk do
